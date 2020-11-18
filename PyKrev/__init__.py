965b2de9e3d49c7aba142d23e9f13f3805227577
kzra <ezrakitson@gmail.com>
2020-11-18 12:43:26 +0000

    Deleted pykrev copy

--- PyKrev/__init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = '0.1.0'
-
-from .formula import *
-from .plotting import *
-from .diversity import *
--- PyKrev/plotting/__init__.py
+++
@@ -1,6 +0,0 @@
-from .multi_van_krevelen_plot import multi_van_krevelen_plot
-from .missing_plot import missing_plot
-from .van_krevelen_histogram import van_krevelen_histogram 
-from .van_krevelen_plot import van_krevelen_plot 
-from .unique_plot import unique_plot
-from .kendrick_mass_defect_plot import kendrick_mass_defect_plot
