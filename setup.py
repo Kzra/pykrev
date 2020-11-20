4db51310c47816256f4d6e25ed61effb625319d0
kzra <ezrakitson@gmail.com>
2020-11-20 15:59:17 +0000

    Updated module jpg

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-from setuptools import setup
+import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-
-setup(name='pykrev',
+setuptools.setup(name='pykrev',
       version='0.1',
       description='van Krevelen style analysis and beyond in python',
       long_description = long_description,
@@ -13,19 +12,19 @@ setup(name='pykrev',
         'License :: OSI approved :: MIT License',
         'Programming Language :: Python :: 3.7',
         'Topic :: Mass spectrometry :: Chemistry',
-      ]
-      keywords = ['mass spectrometry','van Krevelen','cheminformatics']
+      ],
+      keywords = ['mass spectrometry','van Krevelen','cheminformatics'],
       url='https://github.com/Kzra/pykrev',
       author='Ezra Kitson',
       author_email='ezra.kitson@ed.ac.uk',
       license='MIT',
-      packages=setuptools.find_packages(),
+      packages=['pykrev'],
       install_requires=[
           'numpy',
           'pandas',
 	  'matplotlib',
       'Scipy'
       ],
-      python_requires='>=3.6'
-      zip_safe=False
+      python_requires='>=3.6',
+      zip_safe=False,
      )
